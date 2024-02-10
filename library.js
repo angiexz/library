e708248f236703525924aa23f6d729fb3f04c0ba
angiexz <97621483+angiexz@users.noreply.github.com>
2024-02-10 17:29:56 -0500

    set up files

---
+++ library.js
@@ -0,0 +1,20 @@
+const myLibrary =[];
+
+function Book{
+    this.title = title;
+    this.author = author;
+    this.page_no = page_no;
+    this.status = read_book;
+    this.info = function(){
+        return this.title + this.author+this.page_no+this.read_book
+    }
+}
+
+function addBookToLibrary(){
+
+}
+
+//function that loops through array and displays book
+
+
+
