80f9763662fce866b67b478670585c080f2f8094
angiexz <97621483+angiexz@users.noreply.github.com>
2024-02-12 21:07:13 -0500

    set up html and css

--- library.js
+++ library.js
@@ -6,7 +6,7 @@ function Book{
     this.page_no = page_no;
     this.status = read_book;
     this.info = function(){
-        return this.title + this.author+this.page_no+this.read_book
+        return this.title + this.author+this.page_no+this.read_book;
     }
 }
 
